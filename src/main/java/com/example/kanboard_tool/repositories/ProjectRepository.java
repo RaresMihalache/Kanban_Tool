8353f6cde8f2be22a2723830f58f0d48efaa4b50
Rares Mihalache <raresmihalache@yahoo.com>
2022-11-02 20:25:17 +0200

    h2 db integration + project structure + post project api

---
+++ src/main/java/com/example/kanboard_tool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.example.kanboard_tool.repositories;
+
+import com.example.kanboard_tool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> longs);
+}
