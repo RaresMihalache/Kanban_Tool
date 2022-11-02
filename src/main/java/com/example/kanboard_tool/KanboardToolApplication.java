d0efd7b58d5009378ddb0b5d21cf7b03042c4210
Rares Mihalache <raresmihalache@yahoo.com>
2022-11-02 19:39:27 +0200

    first commit

---
+++ src/main/java/com/example/kanboard_tool/KanboardToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.kanboard_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanboardToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanboardToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kanboard_tool/KanboardToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kanboard_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanboardToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
