4b7aa122b608357e16f80c453a45e3bbfa388b7a
Rares Mihalache <raresmihalache@yahoo.com>
2022-11-05 01:45:04 +0200

    delete project by identifier functionality - DELETE

--- src/main/java/com/example/kanboard_tool/services/ProjectService.java
+++ src/main/java/com/example/kanboard_tool/services/ProjectService.java
@@ -37,4 +37,14 @@ public class ProjectService {
     public Iterable<Project> findAllProjects(){
         return projectRepository.findAll();
     }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project = projectRepository.findProjectByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. This project ID does not exist.");
+        }
+
+        projectRepository.delete(project);
+    }
 }
