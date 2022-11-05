0a6d7dc28d57e14365583edf1b264667431fedce
Rares Mihalache <raresmihalache@yahoo.com>
2022-11-06 01:34:40 +0200

    Get Projects - final step

--- react app/ppmtool-react-client/src/components/Dashboard.js
+++ react app/ppmtool-react-client/src/components/Dashboard.js
@@ -11,6 +11,8 @@ class Dashboard extends Component {
   }
 
   render() {
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
@@ -26,7 +28,9 @@ class Dashboard extends Component {
           </div>
         </div>
 
-        <ProjectItem />
+        {projects.map((project) => (
+          <ProjectItem key={project.id} project={project} />
+        ))}
       </div>
     );
   }
