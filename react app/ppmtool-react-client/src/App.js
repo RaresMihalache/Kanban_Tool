8b3ab932eb396636e6f0778a47a37e85d8613851
Rares Mihalache <raresmihalache@yahoo.com>
2022-11-05 16:56:13 +0200

    React router & first funtional component - REACT

--- react app/ppmtool-react-client/src/App.js
+++ react app/ppmtool-react-client/src/App.js
@@ -2,13 +2,21 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Header />}></Route>
+          <Route exact path="/dashboard" element={<Dashboard />}></Route>
+          <Route exact path="/addProject" element={<AddProject />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
